2937a68a9af78a804d7bd2bb1fb18bb8cba04c3f
Erik Mossberg <erkmos@users.noreply.github.com>
2018-09-28 21:22:05 +0700

    inspect a full package.json instead of just a single package

--- index.js
+++ index.js
@@ -5,116 +5,30 @@
 const program = require('commander');
 const packageJson = require('./package.json');
 const moment = require('moment');
-const inquirer = require('inquirer');
 const getPackageDetails = require('./lib/getPackageDetails');
 const walkDependencies = require('./lib/walkDependencies');
-const showImpact = require('./lib/showImpact');
-const showDetails = require('./lib/showDetails');
-const showQuickStats = require('./lib/showQuickStats');
-const colors = require('colors/safe');
-const install = require('./lib/install');
-const exec = require('./lib/exec');
-const getInstallCommand = require(
-  './lib/getInstallCommand'
-);
+const bluebird = require('bluebird');
+const fs = require('fs');
+const { toPairs } = require('lodash');
+const getSimpleTable = require('./lib/getSimpleTable');
 
-/**
- * @param  {string} nameVersion
- * @return {object} name and version loose
- */
-function parseName(nameVersion) {
-  // TODO: check urls
-  let nameVersionStr = String(nameVersion).trim();
-  let scope = false;
-  if (nameVersionStr[0] === `@`) {
-    scope = true;
-    nameVersionStr = nameVersionStr.slice(1);
-  }
-  let [name, versionLoose] = nameVersionStr.split(`@`);
-  if (!versionLoose) {
-    versionLoose = `latest`;
-  }
-  if (scope) {
-    name = `@${name}`;
-  }
-  return { name, versionLoose };
-}
-
-/**
- * @param  {string} command npm or yarn
- * @param  {string[]} args
- * @return {string[]} prompt choices
- */
-function getChoices(command, args) {
-  return [
-    `Install (${colors.bold(`${command} ${args.join(' ')}`)})`,
-    `Impact`,
-    `Details`,
-    `Skip`
-  ];
-}
-
-/**
- * @param  {string} name
- * @param  {string} versionLoose
- * @param  {Object} packages
- */
-function promptNextAction(name, versionLoose, packages) {
-  return getInstallCommand().then(({ command, args }) => {
-    const choices = getChoices(command, args);
-    return inquirer.prompt({
-      type: `list`,
-      name: `next`,
-      message: `What is next?`,
-      choices
-    }).then(({ next }) => {
-      switch (choices.indexOf(next)) {
-        case 0:
-          exec(command, args);
-          return Promise.reject();
-        case 1:
-          return showImpact(name, versionLoose, packages);
-        case 2:
-          return showDetails(packages);
-        default:
-          process.exit(0);
-      }
-    });
-  }).then(() => {
-    return promptNextAction(name, versionLoose, packages);
-  }, (e) => {
-    if (e) {
-      throw e;
-    }
-  });
-}
 
 /**
  * install action
  * @param  {string} nameVersion package considering to install
  */
-function installPackage(nameVersion) {
-  const { name, versionLoose } = parseName(nameVersion);
-  getPackageDetails(name, versionLoose)
-    .then((packageStats) => {
-      process.stdout.cursorTo(0);
-      process.stdout.clearLine(1);
-      process.stdout.write(`${
-        colors.bold(
-          `${packageStats.name}@${packageStats.version}`
-        )
-      } (updated ${
-        moment(packageStats.modified).fromNow()
-      })\n`);
-      return walkDependencies(
+function installPackage(name, versionLoose) {
+  return getPackageDetails(name, versionLoose)
+    .then(async (packageStats) => {
+      const packages = await walkDependencies(
         { [name]: versionLoose }
       );
-    })
-    .then((packages) => {
-      showQuickStats(packages);
-      return promptNextAction(
-        name, versionLoose, packages
-      );
+
+      return [
+        `${packageStats.name}@${packageStats.version}`,
+        moment(packageStats.modified).fromNow(),
+        Object.keys(packages).length,
+      ];
     })
     .catch((e) => {
       console.error(e);
@@ -125,24 +39,29 @@ function installPackage(nameVersion) {
 
 program.version(packageJson.version);
 program.description(packageJson.description);
-program.usage('npm-consider install <pkg>');
+program.usage('npm-consider install <path to package.json>');
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
 }
 
+// feed it package.json path
 program.command(`install [pkg]`)
-  .alias(`i`)
-  .action((pkg, options) => {
-    if (pkg) {
-      installPackage(pkg);
-    } else {
-      install(options);
-    }
-  })
-  .option(`-S, --save`, `Save to dependencies`)
-  .option(`-D, --save-dev`, `Save to devDependencies`)
-  .option(`--production`, `Will not install modules listed in devDependencies`)
-  .option(`--test`, `Exit with code 1 if package limits like maxPackagesNumber or maxSizeBites exceeded`);
+  .action(async (pkg) => {
+    const filename = fs.readFileSync(pkg);
+    const packages = JSON.parse(filename).dependencies;
+    console.log('Walking dependency tree...');
+    const results = await bluebird.map(toPairs(packages), ([name, version]) => {
+      return installPackage(name, version.replace(/[\~\^]/, ''));
+    });
+
+    results.sort((a, b) => (-1) * (a[2] - b[2]));
+
+    const table = getSimpleTable();
+    table.push(['Package', 'Updated', 'Dependencies']);
+    results.forEach(data => table.push(data));
+
+    console.log(`\n${table.toString()}`);
+  });
 
 program.parse(process.argv);
